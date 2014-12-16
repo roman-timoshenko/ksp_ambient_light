f9667999aa299d5b392e2ce0036f7346121dfa2d
Tim Darby <tim.darby.uk@gmail.com>
2014-12-16 14:45:11 +0000

    Updated versioning to a more sensible 1.3.0.1 running on from blizzy's last, and fixed incorrect paths in AVC version file

--- AmbientLightAdjustment/Properties/AssemblyInfo.cs
+++ AmbientLightAdjustment/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.1")]
+[assembly: AssemblyFileVersion("1.3.0.1")]
 
 [assembly: KSPAssemblyDependency("Toolbar", 1, 0)]
